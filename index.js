e7f06813388117a996634b27fdf849d8e223de1b
TheBoringDude <iamcoderx@gmail.com>
2021-06-23 22:38:52 +0800

    impl: `cors`, add client's `ip` in response

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request))
+  event.respondWith(handleMethod(event.request))
 })
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+}
+
 /**
  * Respond with hello worker text
+ * @param {Request} request
+ */
+async function handleOptions(request) {
+  // Make sure the necessary headers are present
+  // for this to be a valid pre-flight request
+  let headers = request.headers
+  if (
+    headers.get('Origin') !== null &&
+    headers.get('Access-Control-Request-Method') !== null &&
+    headers.get('Access-Control-Request-Headers') !== null
+  ) {
+    return new Response(null, {
+      headers: corsHeaders,
+    })
+  } else {
+    // Handle standard OPTIONS request.
+    // If you want to allow other HTTP Methods, you can do that here.
+    return new Response(null, {
+      headers: {
+        Allow: 'GET, HEAD, POST, OPTIONS',
+      },
+    })
+  }
+}
+
+/**
+ * Respond with the user geo info
  * @param {Request} request
  */
 async function handleRequest(request) {
   // modify values
   const info = {
+    ip: request.headers.get('CF-Connecting-IP'),
     geo: {
       latitude: request.cf.latitude,
       longitude: request.cf.longitude,
@@ -24,6 +59,17 @@ async function handleRequest(request) {
   }
 
   return new Response(JSON.stringify(info), {
-    headers: { 'content-type': 'application/json' },
+    headers: { 'content-type': 'application/json', ...corsHeaders },
   })
+}
+
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+async function handleMethod(request) {
+  if (request.method === 'OPTIONS') {
+    return await handleOptions(request)
+  }
+  return await handleRequest(request)
 }
